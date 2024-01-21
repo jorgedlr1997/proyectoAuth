c173c1877759680fafe7efbafa2d3bda10a13c07
jorgedelriego <jorgedelriego76@gmail.com>
2024-01-21 18:27:13 +0100

    change entity user

--- auth/src/main/java/com/springboot/auth/entities/User.java
+++ auth/src/main/java/com/springboot/auth/entities/User.java
@@ -1,6 +1,77 @@
 package com.springboot.auth.entities;
 
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 @Entity
+@Table(name = "users")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	@Size(min = 4, max = 13)
+	private String username;
+
+	@NotEmpty
+	private String password;
+
+	public User() {
+		super();
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, username);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(username, other.username);
+	}
+	
+	
 
 }
